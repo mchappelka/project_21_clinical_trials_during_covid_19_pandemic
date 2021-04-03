341abcb95c8180053cca1fcb3c9b5e40b25b5b99
MIriam Chappelka <cmiriam@sas.upenn.edu>
2021-04-03 15:12:55 -0400

    output results for each trail csv

--- scripts/xml_to_csv.py
+++ scripts/xml_to_csv.py
@@ -12,12 +12,13 @@ import numpy as np
 
 
 
+data_path = "C:/Users/hpfla/OneDrive/Documents/DVRN/clinical_trials/code/project_21_clinical_trials_during_covid_19_pandemic"
+in_path = os.path.join(data_path, "input", "raw")
+out_path = os.path.join(data_path, "output")
 
-data_path = "C:/Users/hpfla/OneDrive/Documents/DVRN/clinical_trials/code/project_21_clinical_trials_during_covid_19_pandemic/input/raw"
 
 
-
-xml_folder = os.path.join(data_path, "search_result")
+xml_folder = os.path.join(in_path, "search_result")
 
 
 xmlfiles = os.listdir(xml_folder)
@@ -54,27 +55,34 @@ for idx,filename in enumerate(xmlfiles):
             count = p.attrib["count"]
             mydf = create_col(mydf, newcol)
             mydf[newcol] = count
-
-    dfs.append(mydf.copy())
-
-            
-    '''try:
-        for reason in dom.find("clinical_results/participant_flow/period_list/period/drop_withdraw_reason_list"):
-            print(reason.find("title").text)
-            participants = reason.find("participants_list")
+    try:
+        for r in dom.find("clinical_results/participant_flow/period_list/period/drop_withdraw_reason_list"):
+            reason = r.find("title").text
+            participants = r.find("participants_list")
             for p in participants:
-                print(p.attrib["group_id"] + ": " + p.attrib["count"])
+                #print(p.attrib["group_id"] + ": " + p.attrib["count"])
+                newcol = (reason + p.attrib["group_id"]).lower()
+                count = p.attrib["count"]
+                mydf = create_col(mydf, newcol)
+                mydf[newcol] = count
     except:
         print(f"Withdrawal reasons {filename} are not included")
         
+
+    
     for measure in dom.find("clinical_results/baseline/measure_list"):
         print(" ")
         print(measure.find("title").text)
         if measure.find("title").text == "Region of Enrollment":
             for country in measure.find("class_list"):
+                country_str = country.find("title").text
                 print(country.find("title").text)
                 for count in country.find("category_list/category/measurement_list"):
                     print(count.attrib["group_id"] + ": " + count.attrib["value"])
+                    newcol = (country_str + count.attrib["group_id"]).lower()
+                    count = count.attrib["value"]
+                    mydf = create_col(mydf, newcol)
+                    mydf[newcol] = count
         elif measure.find("title").text.lower() == "disease severity":
             print("come back to this later")
         else:
@@ -84,8 +92,16 @@ for idx,filename in enumerate(xmlfiles):
                             print(submeasure.find("title").text)
                             for count in submeasure.find("measurement_list"):
                                 print(count.attrib["group_id"] + ": " + count.attrib["value"])
+                                newcol = submeasure.find("title").text + count.attrib["group_id"].lower()
+                                count = count.attrib["value"]
+                                mydf = create_col(mydf, newcol)
+                                mydf[newcol] = count
             except: 
-                print(measure.find("title").text)'''
+                print(measure.find("title").text)
+    dfs.append(mydf.copy())
 
 
 final_df = pd.concat(dfs)
+print(final_df.columns)
+
+final_df.to_csv(os.path.join(out_path, "processed_trials.csv"))
